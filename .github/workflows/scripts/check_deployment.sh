58d4226b752aaeb9dd49d837736e1f93e1fd8d6f
RAJKUMAR BUDDHA <rajkumarbuddha@RAJKUMARs-MacBook-Pro.local>
2024-03-14 14:40:03 +0530

    Log the error message

--- .github/workflows/scripts/check_deployment.sh
+++ .github/workflows/scripts/check_deployment.sh
@@ -12,11 +12,11 @@ if [ $FORCED_DEPLOYMENT_TO_PROD_AND_SC == "true" ]; then
 fi
 
 if [ $CURRENT_WEEK_DAY -ge $PAUSE_DAY_OF_THE_WEEK ]; then
-  echo "Cannot deploy on $(TZ="Asia/Kolkata" date +%A)" >&2
+  echo "::error::Cannot deploy on $(TZ="Asia/Kolkata" date +%A)" 
   exit 1
 fi
 
 if [[ $CURRENT_TIME < $DAY_START_TIME || $CURRENT_TIME > $DAY_END_TIME ]]; then
-  echo "Deployment only happens between $DAY_START_TIME to $DAY_END_TIME" >&2
+  echo "::error::Deployment only happens between $DAY_START_TIME to $DAY_END_TIME"
   exit 2
 fi
